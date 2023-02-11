f66a64df7f464098a39a635a157be3fe0028413a
QBert <cubert.p.rickles@gmail.com>
2023-02-11 15:49:42 -0500

    add ability to change time to current time, get the current location of the sun for null island

--- skywatcher/base/src/test/java/me/qbert/skywatch/TestAstroCalculators.java
+++ skywatcher/base/src/test/java/me/qbert/skywatch/TestAstroCalculators.java
@@ -1,6 +1,7 @@
 package me.qbert.skywatch;
 
 
+import java.util.Calendar;
 import java.util.TimeZone;
 
 import org.apache.logging.log4j.Level;
@@ -18,6 +19,7 @@ import me.qbert.skywatch.astro.impl.StarObject;
 import me.qbert.skywatch.astro.impl.SunObject;
 import me.qbert.skywatch.exception.UninitializedObject;
 import me.qbert.skywatch.model.CelestialAddress;
+import me.qbert.skywatch.model.ObjectDirectionAltAz;
 import me.qbert.skywatch.model.ObjectDirectionRaDec;
 
 /*
@@ -43,6 +45,54 @@ public class TestAstroCalculators {
 	private void assertForTest(String failMessageRa, String failMessageDec, ObjectDirectionRaDec computedDirection, double expectedRa, double expectedDec) throws AssertionFailedError {
 		assertForTest(failMessageRa, computedDirection.getRightAscension(), expectedRa);
 		assertForTest(failMessageDec, computedDirection.getDeclination(), expectedDec);
+	}
+	
+	private ObjectDirectionAltAz raDeclinationToAltitudeAzimuth(double rightAscension, double declination,
+			double latitude, double longitude) {
+		double useLat = latitude;
+		while ((useLat > 89.999) && (useLat < 90.001))
+			useLat -= 0.0001;
+		while ((useLat < -89.999) && (useLat > -90.001))
+			useLat += 0.0001;
+		double latRad = Math.toRadians(useLat);
+		double raRad = Math.toRadians(rightAscension);
+		double decRad = Math.toRadians(declination);
+
+		double alt = Math.toDegrees(
+				Math.asin(Math.sin(latRad) * Math.sin(decRad) + Math.cos(latRad) * Math.cos(decRad) * Math.cos(raRad)));
+		double az = (Math.toDegrees(Math.atan2(-1 * Math.sin(raRad),
+				Math.tan(Math.toRadians(declination)) * Math.cos(Math.toRadians(useLat))
+						- Math.sin(Math.toRadians(useLat)) * Math.cos(raRad)))
+				+ 360.0) % 360.0;
+
+		ObjectDirectionAltAz coord = new ObjectDirectionAltAz();
+		coord.setAltitude(alt);
+		coord.setAzimuth(az);
+
+		return coord;
+	}
+
+	
+	private String angleToDms(double angle) {
+		int h = (int)(angle);
+		angle -= h;
+		angle *= 60;
+		int m = (int)(angle);
+		angle -= m;
+		angle *= 60;
+		return String.format("%02dd %02dm %02fs", h, m, angle);
+	}
+	
+	private String angleToHms(double angle) {
+		angle /= 15.0;
+		
+		int h = (int)(angle);
+		angle -= h;
+		angle *= 60;
+		int m = (int)(angle);
+		angle -= m;
+		angle *= 60;
+		return String.format("%02dh %02dm %02fs", h, m, angle);
 	}
 	
 	private void testTransactional() throws UninitializedObject {
@@ -134,6 +184,32 @@ public class TestAstroCalculators {
 		direction = starObjTwo.getCurrentDirection();
 		System.out.println("	Star(+4.0, -6.0) = " + direction.getRightAscension() + ", " + direction.getDeclination());
 		assertForTest("Star 2 RA is incorrect", "Star 2 Dec is incorrect", direction, 108.81889224944868, 39.0);
+		
+		System.out.println("Additional... where is the sun now? ");
+		lat=45.41212;
+		lon=-75.69026;
+		transactionalListener.begin();
+		time.setCurrentTime();
+		myLocation.setGeoLocation(lat, lon);
+		transactionalListener.commit();
+		
+		Calendar workingCal = time.getTime();
+		year = workingCal.get(Calendar.YEAR);
+		month = workingCal.get(Calendar.MONTH) + 1;
+		day = workingCal.get(Calendar.DAY_OF_MONTH);
+		hour = workingCal.get(Calendar.HOUR_OF_DAY);
+		min = workingCal.get(Calendar.MINUTE);
+		sec = workingCal.get(Calendar.SECOND);
+		double dstOffset = (double)(workingCal.get(Calendar.DST_OFFSET) / 3600000.0);
+		double tzOffset = (double)(workingCal.get(Calendar.ZONE_OFFSET) / 3600000.0);
+		double timezoneAdjust = tzOffset + dstOffset;
+		
+		direction = sun.getCurrentDirection();
+		System.out.println("computePosition for: lat=" + lat + ", lon=" + lon +
+				"   AT " + String.format("%04d-%02d-%02d %02d:%02d:%02d", year, month, day, hour, min, sec));
+		System.out.println("	 Sun = " + angleToHms(direction.getRightAscension()) + " LHA, " + angleToDms(direction.getDeclination()) + " dec");
+		ObjectDirectionAltAz altAz = raDeclinationToAltitudeAzimuth(direction.getRightAscension(), direction.getDeclination(), lat, lon);
+		System.out.println(" Altitude: " + angleToDms(altAz.getAltitude()) + " and azimuth: " + angleToDms(altAz.getAzimuth()));
 	}
 	
 	public static void main(String[] args) throws UninitializedObject {
