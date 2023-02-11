fe8f451d627a1e7093fdc9c312d6aafec2e53cf3
QBert <cubert.p.rickles@gmail.com>
2023-02-11 15:49:43 -0500

    update pom version, add a time bias (or current time shift) to the time, add sun and moon precession calculators

--- skywatcher/base/src/main/java/me/qbert/skywatch/astro/ObservationTime.java
+++ skywatcher/base/src/main/java/me/qbert/skywatch/astro/ObservationTime.java
@@ -48,11 +48,14 @@ public class ObservationTime {
 		Calendar calendar = Calendar.getInstance();
 		
 		this.time.setTimeInMillis(calendar.getTimeInMillis() + (long)timeBiasMillis);
+<<<<<<< HEAD
 		recompute();
 	}
 	
 	public void addTime(int units, int calendarClassField) throws UninitializedObject {
 		this.time.add(calendarClassField, units);
+=======
+>>>>>>> 71e1364 (update pom version, add a time bias (or current time shift) to the time, add sun and moon precession calculators)
 		recompute();
 	}
 	
