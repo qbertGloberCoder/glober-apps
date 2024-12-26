6d3a7426b58b9994ed0c7ef5a8ced8af47d683fd
QBert <cubert.p.rickles@gmail.com>
2024-12-26 11:01:34 -0500

    add a split screen renderer and a mercator scroll image renderer

---
+++ skywatcher/earthclock/src/main/java/me/qbert/skywatch/ui/renderers/MercatorScrollImageRenderer.java
@@ -0,0 +1,88 @@
+package me.qbert.skywatch.ui.renderers;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import me.qbert.ui.renderers.AbstractImageRenderer;
+
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+public class MercatorScrollImageRenderer extends AbstractImageRenderer {
+	private BufferedImage mercatorImage;
+	private BufferedImage lastBi;
+	
+	private double lastLon;
+
+	public MercatorScrollImageRenderer(File mercatorImageFile) {
+		if (mercatorImageFile != null) {
+			try {
+				mercatorImage = loadImageFromFile(mercatorImageFile);
+			} catch (Exception e) {
+				e.printStackTrace();
+				mercatorImage = null;
+			}
+		} else {
+			mercatorImage = null;
+		}
+	}
+
+	
+	public void wrapToCoordinates(double longitude) {
+		if ((lastBi != null) && (longitude == lastLon))
+			return;
+		
+		
+		int inWidth = mercatorImage.getWidth();
+		int inHeight = mercatorImage.getHeight();
+		
+		int outWidth = inWidth;
+		int outHeight = inHeight;
+		
+		lastBi = new BufferedImage(outWidth, outHeight, BufferedImage.TYPE_INT_ARGB);
+		
+		Graphics2D g2d = lastBi.createGraphics();
+		
+		while (longitude < -180.0)
+			longitude += 360.0;
+		while (longitude > 180.0)
+			longitude -= 360.0;
+		
+		if (longitude < 0.0) {
+			int sourceSplit = inWidth - (int)((double)inWidth * (0.0 - longitude) / 360);
+			if (sourceSplit == 0)
+				g2d.drawImage(mercatorImage, 0, 0, outWidth, outHeight, 0, 0, inWidth, inHeight, null);
+			else {
+				g2d.drawImage(mercatorImage, 0, 0, (outWidth - sourceSplit), outHeight, sourceSplit, 0, inWidth, inHeight, null);
+				g2d.drawImage(mercatorImage, (outWidth - sourceSplit), 0, outWidth, outHeight, 0, 0, sourceSplit, inHeight, null);
+			}
+		} else {
+			int sourceSplit = (int)((double)inWidth * longitude / 360);
+			if (sourceSplit == 0)
+				g2d.drawImage(mercatorImage, 0, 0, outWidth, outHeight, 0, 0, inWidth, inHeight, null);
+			else {
+				g2d.drawImage(mercatorImage, 0, 0, (outWidth - sourceSplit), outHeight, sourceSplit, 0, inWidth, inHeight, null);
+				g2d.drawImage(mercatorImage, (outWidth - sourceSplit), 0, outWidth, outHeight, 0, 0, sourceSplit, inHeight, null);
+			}
+		}
+		
+	    g2d.dispose();
+		
+	    lastLon = longitude;
+	    
+	    setOriginalImage(lastBi);
+	}
+}
